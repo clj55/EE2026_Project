c89fa1990ba15716dd266c80ba7bac9fde8f6c9e
clj55 <103747880+clj55@users.noreply.github.com>
2024-10-07 00:12:00 +0800

    subtask C
    Still need to check pixel size, and time it waits

--- MODS.xpr/MODS/MODS.srcs/sources_1/new/Top_Student.v
+++ MODS.xpr/MODS/MODS.srcs/sources_1/new/Top_Student.v
@@ -42,12 +42,15 @@ module draw_oled(input clk, input [12:0] pixel_index, output reg [15:0] oled_dat
     input btnC, input btnU,
     input onehz, input thirtyhz, input fiftyhz);
     
-    parameter BLACK_TOP = 14; 
+    parameter BLACK_TOP = 15; 
     parameter BLACK_BOTTOM = 49;
-    parameter SQUARE_SIZE = 14;
+    parameter BLACK_SIDE = 80;
+    parameter SQUARE_SIZE = 15;
+    parameter MAX_X = 96;
+    parameter MAX_Y = 64;
     
     wire [7:0] x; wire [7:0]y; wire [15:0] else_colour;  wire [15:0]first_colour;
-    reg green_first; reg start; reg [2:0]state; reg waiting; reg [1:0] wait_count; wire draw_first;
+    wire green_first; reg start; reg [2:0]state; reg waiting; reg [1:0] wait_count; wire draw_first;
     reg increment; wire wait_done; 
     reg [7:0] x_green; reg [7:0] y_green; reg [7:0] x_red; reg [7:0]y_red; reg [7:0]in_var; 
     wire [15:0] red; wire [15:0] green; wire [15:0] black;  
@@ -57,32 +60,32 @@ module draw_oled(input clk, input [12:0] pixel_index, output reg [15:0] oled_dat
     assign red = 16'hF800; assign green = 16'h07E0; assign black = 0;
     assign x = pixel_index % 96;
     assign  y = pixel_index / 96;    
+    assign green_first = (state <= 2 || state == 7) ? 1 : 0;
     
-    assign draw_first = green_first ? green_L : red_L;
-    assign first_colour = green_first ? green : ((start) ? black : red);
-    assign else_colour = start ?  black : (green_first ? red : green);
-    assign green_L = ((x <= x_green) && (y <= y_green));
-    assign red_L = ((x <= x_red) && (y >= y_red));  
+    assign draw_first = green_first ? green_L : red_L; //switch when hit bottom right before wait 
+    assign first_colour = green_first ? green : (start ? black : red); //black when bottom right 
+    assign else_colour = green_first ?  (start ? black : red) : green;
+    assign green_L = (x < x_green) && ((y < BLACK_TOP) || (x > BLACK_SIDE  && y < y_green));
+    assign red_L = (x < x_red) && (y > BLACK_BOTTOM || (x > BLACK_SIDE && y > y_red));  
     
     assign counter_clk = (state <= 3) ? thirtyhz : fiftyhz;
 
     count_wait w (.enable(waiting), .clk(onehz), .wait_done(wait_done));
-    incrementer greenx (.enable(!waiting && (state == 1 || state == 6)), .increment(increment), .clk(counter_clk), .in_var(x_green), .out_var(outxgreen));
+    incrementer greenx (.enable(!waiting && (state == 1 || state == 7)), .increment(increment), .clk(counter_clk), .in_var(x_green), .out_var(outxgreen));
     incrementer greeny (.enable(!waiting && state == 2), .increment(increment), .clk(counter_clk), .in_var(y_green), .out_var(outygreen));
-    incrementer redx (.enable(!waiting && (state == 3 || state == 4)), .increment(increment), .clk(counter_clk), .in_var(x_red), .out_var(outxred));
-    incrementer redy (.enable(!waiting && state == 5),  .increment(increment), .clk(counter_clk), .in_var(y_red), .out_var(outyred));
+    incrementer redx (.enable(!waiting && (state == 3 || state == 5)), .increment(increment), .clk(counter_clk), .in_var(x_red), .out_var(outxred));
+    incrementer redy (.enable(!waiting && state == 6),  .increment(increment), .clk(counter_clk), .in_var(y_red), .out_var(outyred));
     
     initial begin 
         state = 0; //stop, green right, green down, red right, red up 
         x_green = SQUARE_SIZE; // x right
         y_green = BLACK_TOP; // y bottom
-        x_red = 95;  // x right
+        x_red = BLACK_SIDE;  // x right
         y_red = BLACK_BOTTOM; // y top 
         wait_count = 0;
         waiting = 0;
         start = 1;
         oled_data = 0;
-        green_first = 1;
         increment = 1;
         in_var = x_green;
     end
@@ -96,16 +99,15 @@ module draw_oled(input clk, input [12:0] pixel_index, output reg [15:0] oled_dat
  
        else begin
             if (state == 0) begin
-                x_green <= 14;
-                y_green <= 14;
-                green_first <= 1;
+                x_green <= SQUARE_SIZE;
+                y_green <= SQUARE_SIZE;
                 if (btnC) begin //wait for btnC press
                     state <= 1;
                 end
             end 
             else if (state == 1) begin // green move right
                 increment <= 1;
-                if (x_green == 95) begin 
+                if (x_green == MAX_X) begin 
                     state <= 2;
                     waiting <= 1;
                 end 
@@ -113,10 +115,11 @@ module draw_oled(input clk, input [12:0] pixel_index, output reg [15:0] oled_dat
                     x_green <= outxgreen;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
                 end
             end else if (state == 2) begin // green move down
-                if (y_green == 63) begin 
+                if (y_green == MAX_Y) begin 
                     state <= 3;
                     waiting <= 1;
-                    green_first <= 0;
+                    y_red <= BLACK_BOTTOM;
+                    x_red <= BLACK_SIDE + 1;
                 end else begin        
                     y_green <= outygreen; //y_green + 1               
                 end
@@ -124,33 +127,37 @@ module draw_oled(input clk, input [12:0] pixel_index, output reg [15:0] oled_dat
             end else if (state == 3) begin // green move left 
                 increment <= 0;
                 if (x_red == 0) begin 
-                    state <= 7;
-                    waiting <= 1;
-                    x_red <= 14; //red square appear
-                    y_red <= BLACK_BOTTOM;
-                    y_green <= BLACK_BOTTOM;
                     start <= 0;
+                    state <= 4;
+                    waiting <= 1;
                 end else begin  
                     x_red <= outxred; // x_red - 1 
                 end
-            end else if (state == 7 && btnU) begin // wait for user press btnU
-                state <= 4;       
-            end else if (state == 4) begin // red move right
-                increment <= 1;
-                if (x_red == 95) begin 
+            end else if (state == 4) begin // wait for user press btnU                  
+                x_red <= SQUARE_SIZE; //red square appear  
+                y_green <= BLACK_BOTTOM;
+                if (btnU) begin 
                     state <= 5;
+                end
+            end else if (state == 5) begin // red move right
+                increment <= 1;
+                if (x_red == MAX_X) begin 
+                    state <= 6;
                 end else begin  
                     x_red <= outxred; //x_red + 1
                 end
-            end else if (state == 5) begin // red move up
+            end else if (state == 6) begin // red move up
                 increment <= 0;
+                y_green <= SQUARE_SIZE; //reset y_green
+                x_green <= BLACK_SIDE; // reset x_green
                 if (y_red == 0) begin 
-                    state <= 6;
-                    y_green <= 14;
+                    state <= 7;
                 end else begin  
                     y_red <=  outyred; //y_red - 1
                 end
-            end else if (state == 6) begin // red move left
+            // green first ------------------------------------------
+            end else if (state == 7) begin // red move left
+                increment <= 0;
                 if (x_green == 0) begin 
                     state <= 0;
                 end else begin
@@ -162,7 +169,7 @@ module draw_oled(input clk, input [12:0] pixel_index, output reg [15:0] oled_dat
     
     
     always @(posedge clk) begin 
-        if ((x<=80) && (y >= 15 && y <= 48)) begin //Black Rectangle
+        if ((x <= BLACK_SIDE) && (y >= BLACK_TOP && y <= BLACK_BOTTOM)) begin //Black Rectangle
             oled_data <= 0;
         end
         
@@ -183,9 +190,12 @@ module incrementer (input enable, input increment, input clk, input [7:0]in_var,
     always @(posedge clk) begin
         if (enable) begin 
             out_var <= (increment) ? in_var + 1 : in_var - 1;
+        end else begin 
+            out_var <= in_var;
         end
     end
 endmodule
+
 
 module count_wait (input enable, input clk, output reg wait_done);
     reg [1:0]wait_count = 0;
